7feb5cf2098c9ac8ae021ccf14def95945833278
Dhanshree9787 <158804166+Dhanshree9787@users.noreply.github.com>
2025-06-12 20:04:36 +0000

    conversion of datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "chikki@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "hc@hc.com"
+accountCity = "mumbai"
+accountPassword = "121212"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
